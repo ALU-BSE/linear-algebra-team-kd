375efca84fe59794b3b97db66633ddc187bdd4ac
Keza <144003584+jkeza1@users.noreply.github.com>
2025-01-21 00:15:05 +0200

    initialization the prices of array

--- matrix_mul.py
+++ matrix_mul.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import numpy as np
 
-# Load data
-data = pd.read_csv('path/to/data')
-
 # Example arrays
 Prices = [[300, 500],
           [1000, 120.85]]
+
 
 Array2 = [200, 100]
 
